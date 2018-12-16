e39a473a00dcb6498b9a3b37ca0a24a920f7a557
Caina <caina27@gmail.com>
2018-12-16 12:28:26 +0100

    [DC] help command using buffer

--- app/commands/help/help.go
+++ app/commands/help/help.go
@@ -1,15 +1,17 @@
 package help
 
 import (
+	"bytes"
 	"github.com/parking-lot/app/config"
 )
 
-func Command(teste *config.Client) {
+func Command(client *config.Client) {
+	buffer := bytes.Buffer{}
+	buffer.WriteString("list of commands available \n")
+	buffer.WriteString("-- init \n")
+	buffer.WriteString("-- status | list of cars parked \n")
+	buffer.WriteString("-- park {plate color} | park the car on the slot nearby the door \n")
+	buffer.WriteString("-- leave {ticket-id} | remove the car from the park \n")
 
-	teste.Send <- "message"
-	//fmt.Println("list of commands available")
-	//fmt.Println("-- init")
-	//fmt.Println("-- status | list of cars parked")
-	//fmt.Println("-- park {plate color} | park the car on the slot nearby the door")
-	//fmt.Println("-- leave {ticket-id} | remove the car from the park")
+	client.Send <- buffer.String()
 }
