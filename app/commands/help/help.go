9d75e61a859b933b5e6e17159f5ad66e03eafc2f
Caina <caina27@gmail.com>
2018-12-19 23:30:48 +0100

    [DC] Updating the Readme and the help command pallet

--- app/commands/help/help.go
+++ app/commands/help/help.go
@@ -8,9 +8,9 @@ import (
 func Command(client *client.Client) {
 	buffer := bytes.Buffer{}
 	buffer.WriteString("list of commands available \n")
-	buffer.WriteString("-- initialization \n")
+	buffer.WriteString("-- init {number of slots} \n")
 	buffer.WriteString("-- status | list of cars parked \n")
-	buffer.WriteString("-- park {plate color} | park the car on the slot nearby the door \n")
+	buffer.WriteString("-- park {plate} {color} | park the car if we have any space left \n")
 	buffer.WriteString("-- leave {ticket-id} | remove the car from the park \n")
 
 	client.Send <- buffer.String()
